f03ba4659425198b85fb4a1be7ad27cb566735fd
HumbleBee14 <dsyd14@gmail.com>
2021-06-16 18:31:56 +0530

    Redis Cache Implementation

---
+++ RedisCacheNodejs/RedisDemo1/README.md
@@ -0,0 +1,23 @@
+Install Redis
+
+wget https://download.redis.io/releases/redis-6.0.9.tar.gz
+tar xzf redis-6.0.9.tar.gz
+cd redis-6.0.9
+make
+make install
+
+
+For Windows: Install from here and run redis-server
+https://github.com/microsoftarchive/redis/releases/tag/win-3.2.100
+and set path 
+
+and start the redis server using redis-server
+
+npm install --save  express redis axios
+
+
+then check the results on Postman -
+ Testing above APi using: GET request to : http://localhost:3000/jobs?search=node.js
+
+ My results:
+ 
---
+++ RedisCacheNodejs/RedisDemo1/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const axios = require("axios");
+const redis = require("redis");
+const app = express();
+
+const redisPort = 6379; // default port of redis
+const client = redis.createClient(redisPort);
+
+client.on("error", (err) => {
+    console.log(err);
+})
+
+app.get("/jobs", (req, res) => {
+    const searchTerm = req.query.search;
+    try {
+        client.get(searchTerm, async (err, jobs) => {
+            if (err) throw err;
+    
+            if (jobs) {
+                res.status(200).send({
+                    jobs: JSON.parse(jobs),
+                    message: "data retrieved from the cache"
+                });
+            }
+            else {
+                const jobs = await axios.get(`https://jobs.github.com/positions.json?search=${searchTerm}`);
+                client.setex(searchTerm, 600, JSON.stringify(jobs.data));
+                res.status(200).send({
+                    jobs: jobs.data,
+                    message: "cache miss"
+                });
+            }
+        });
+    } catch(err) {
+        res.status(500).send({message: err.message});
+    }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Node server started !!");
+});
+
+// Testing above APi using: GET request to : http://localhost:3000/jobs?search=node.js
